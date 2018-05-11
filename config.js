04bc9acb1d7017bbaa389c4224a215446027d523
unknown <ajameslusk@gmail.com>
2018-05-11 18:02:02 -0400

    set up heroku

--- config.js
+++ config.js
@@ -6,6 +6,8 @@ exports.MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/noteful';
 
 exports.TEST_MONGODB_URI = process.env.TEST_MONGODB_URI || 'mongodb://localhost/noteful-test';
 
+exports.CLOUD_MONGODB_URI = 'mongodb://username:password@ds121730.mlab.com:21730/noteful-v4';
+
 exports.JWT_SECRET = process.env.JWT_SECRET;
 
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
